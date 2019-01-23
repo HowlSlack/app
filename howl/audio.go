74253c7a005424920ed23c32fbc5ae28c5b1df0b
Aksels Ledins <aksels.ledins@gmail.com>
2019-01-23 18:45:38 +0100

    audio : handle different file formats and resample

--- howl/audio.go
+++ howl/audio.go
@@ -3,17 +3,18 @@ package howl
 import (
 	"io"
 	"log"
+	"time"
 
 	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
 	"github.com/faiface/beep/vorbis"
 )
 
 // InitAudio inits the audio speaker
 func InitAudio() {
-	// the sample rate is set to 12000 as format.SampleRate does
-	// not return an accurate value
-	err := speaker.Init(12000, 12000)
+	sr := beep.SampleRate(22050)
+	err := speaker.Init(sr, sr.N(time.Second/2))
 	if err != nil {
 		log.Fatal("audio: could not init speaker")
 		return
@@ -21,18 +22,35 @@ func InitAudio() {
 }
 
 // PlayAudioStream will output a stream to device speakers
-func PlayAudioStream(as io.ReadCloser) {
+func PlayAudioStream(as io.ReadCloser, format string) {
 	// we decode the ogg stream
-	s, _, _ := vorbis.Decode(as)
+	var s beep.StreamSeekCloser
+	var f beep.Format
+
+	if format == "vorbis" {
+		s, f, _ = vorbis.Decode(as)
+	} else if format == "mp3" {
+		s, f, _ = mp3.Decode(as)
+	}
 
 	// channel, which will signal the end of the playback.
 	playing := make(chan struct{})
 
-	// play the stream on the speaker
-	speaker.Play(beep.Seq(s, beep.Callback(func() {
-		// call back when the stream ended
-		close(playing)
-	})))
+	if format == "mp3" {
+		// play the stream on the speaker
+		speaker.Play(beep.Seq(beep.Resample(9, f.SampleRate, beep.SampleRate(22050), s), beep.Callback(func() {
+			// call back when the stream ended
+			close(playing)
+		})))
+	}
+
+	if format == "vorbis" {
+		// play the stream on the speaker
+		speaker.Play(beep.Seq(beep.Resample(9, f.SampleRate, beep.SampleRate(44000), s), beep.Callback(func() {
+			// call back when the stream ended
+			close(playing)
+		})))
+	}
 
 	// wait for the end of the stream
 	<-playing
